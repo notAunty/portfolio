1dcb075ff4a0433f1b5ce7c69be64b5dbde325cb
aso <5123070-notaunty@users.noreply.gitlab.com>
2021-07-22 17:31:56 +0800

    fixed go-to-top button visibility

--- js/main.js
+++ js/main.js
@@ -1,75 +1,88 @@
-  const d = new Date();
-  const hours = d.getHours();
-  const nightTime = hours >= 19 || hours <= 7; // between 7pm and 7am
-  const body = document.querySelector('body');
-  const toggle = document.getElementById('toggle');
-  const input = document.getElementById('switch');
+$('document').ready(main);
 
-  if (nightTime) {
-    input.checked = true;
-    body.classList.add('night');
-  }
+function main() {
+  // NIGHT MODE
+  // const d = new Date();
+  // const hours = d.getHours();
+  // const nightTime = hours >= 19 || hours <= 7; // between 7pm and 7am
+  const body = document.querySelector("body");
+  const toggle = document.getElementById("toggle");
+  const input = document.getElementById("switch");
 
-  toggle.addEventListener('click', function() {
+  // if (nightTime) {
+  input.checked = true;
+  body.classList.add("night");
+  // }
+
+  toggle.addEventListener("click", function () {
     const isChecked = input.checked;
     if (isChecked) {
-      body.classList.remove('night');
+      body.classList.remove("night");
     } else {
-      body.classList.add('night');
+      body.classList.add("night");
     }
   });
 
-  const introHeight = document.querySelector('.intro').offsetHeight;
-  const topButton = document.getElementById('top-button');
-  const $topButton = $('#top-button');
+  // GO TO TOP BUTTON
+  const introHeight = document.querySelector(".intro").offsetHeight;
+  const goTopButton = document.getElementById("go-top-btn");
+  const $goTopButton = $("#go-top-btn");
 
   window.addEventListener(
-    'scroll',
-    function() {
+    "scroll",
+    function () {
       if (window.scrollY > introHeight) {
-        $topButton.fadeIn();
+        $goTopButton.fadeIn();
       } else {
-        $topButton.fadeOut();
+        $goTopButton.fadeOut();
       }
     },
     false
   );
 
-  topButton.addEventListener('click', function() {
-    $('html, body').animate({ scrollTop: 0 }, 500);
+  goTopButton.addEventListener("click", function () {
+    $("html, body").animate({ scrollTop: 0 }, 500);
   });
 
-  const hand = document.querySelector('.emoji.wave-hand');
+  // BROKEN IMAGES
+   document.querySelectorAll('img').forEach(function(img){
+    img.onerror = function(){this.style.display='none';};
+   })
+
+  // HAND WAVE ANIMATION
+  const hand = document.querySelector(".emoji.wave-hand");
 
   function waveOnLoad() {
-    hand.classList.add('wave');
-    setTimeout(function() {
-      hand.classList.remove('wave');
+    hand.classList.add("wave");
+    setTimeout(function () {
+      hand.classList.remove("wave");
     }, 2000);
   }
 
-  setTimeout(function() {
+  setTimeout(function () {
     waveOnLoad();
   }, 1000);
 
-  hand.addEventListener('mouseover', function() {
-    hand.classList.add('wave');
+  hand.addEventListener("mouseover", function () {
+    hand.classList.add("wave");
   });
 
-  hand.addEventListener('mouseout', function() {
-    hand.classList.remove('wave');
+  hand.addEventListener("mouseout", function () {
+    hand.classList.remove("wave");
   });
 
   window.sr = ScrollReveal({
     reset: false,
     duration: 600,
-    easing: 'cubic-bezier(.694,0,.335,1)',
+    easing: "cubic-bezier(.694,0,.335,1)",
     scale: 1,
     viewFactor: 0.3,
   });
 
-  sr.reveal('.background');
-  sr.reveal('.skills');
-  sr.reveal('.experience', { viewFactor: 0.2 });
-  sr.reveal('.featured-projects', { viewFactor: 0.1 });
-  sr.reveal('.other-projects', { viewFactor: 0.05 });
+  sr.reveal(".background");
+  sr.reveal(".skills");
+  sr.reveal(".experience", { viewFactor: 0.2 });
+  sr.reveal(".featured-projects", { viewFactor: 0.1 });
+  sr.reveal(".other-projects", { viewFactor: 0.05 });
+}
+
